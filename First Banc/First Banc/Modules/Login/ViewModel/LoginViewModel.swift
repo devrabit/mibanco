81ad30e8efabb8f54ee2b9a1a3137de7f4bbe09a
acarreno <jeison-c13@hotmail.com>
2019-10-07 18:13:21 -0500

    primer commit

---
+++ First Banc/First Banc/Modules/Login/ViewModel/LoginViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewModel.swift
+//  First Banc
+//
+//  Created by iMac on 10/7/19.
+//  Copyright © 2019 Wigilabs. All rights reserved.
+//
+
+import Foundation
---
+++ First Banc/First Banc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First Banc
+//
+//  Created by iMac on 10/7/19.
+//  Copyright © 2019 Wigilabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
